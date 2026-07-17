9b794b094dfd23b884145b8e278e687e67c4346b
Marwan <sm.astana0102@gmail.com>
2026-07-17 22:00:38 +0500

    initial commit

---
+++ static/script.js
@@ -0,0 +1,14 @@
+const eventDate = new Date("August 20, 2026 10:00:00").getTime();
+
+setInterval(() => {
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    document.getElementById("days").innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("hours").innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    document.getElementById("minutes").innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    document.getElementById("seconds").innerText = Math.floor((distance % (1000 * 60)) / 1000);
+}, 1000);
+function toggleTheme() {
+    document.body.classList.toggle('light-theme');
+}
